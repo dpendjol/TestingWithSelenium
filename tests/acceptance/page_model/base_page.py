cd849b92e5f2ae8e6994a86ac1e0d250c5909406
Dominique Pendjol <d.pendjol@gmail.com>
2021-04-19 19:45:44 +0200

    rewritten de link, made it more useable

--- tests/acceptance/page_model/base_page.py
+++ tests/acceptance/page_model/base_page.py
@@ -7,6 +7,10 @@ class BasePage:
     @property
     def url(self):
         return 'http://127.0.0.1:5000'
+    
+    @property
+    def links(self):
+        return self.driver.find_elements(*BasePageLocators.LINKS)
 
     @property
     def title(self):
--- tests/acceptance/steps/interactions.py
+++ tests/acceptance/steps/interactions.py
@@ -1,8 +1,16 @@
 from behave import *
+from tests.acceptance.page_model.base_page import BasePage
 
 use_step_matcher('re')
 
-@when('I click on the link with id "(.*)"')
-def step_impl(context, link_id):
-    link = context.driver.find_element_by_id(link_id)
-    link.click()
+@when('I click on the "(.*)" link')
+def step_impl(context, link_text):
+    page = BasePage(context.driver)
+    links = page.links
+    
+    found_links = [l for l in links if l.text == link_text]
+    
+    if len(found_links) > 0:
+        found_links[0].click()
+    else:
+        raise RuntimeError()
